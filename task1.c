bdfce06c11683a2d98fffadf7ad0fbee294d1b91
Muhammad Umair Kamal <umairkamal2007@gmail.com>
2026-09-14 00:16:27 +0500

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int units = 650;
+
+    if (units > 500) {
+        printf("High Usage\n");
+    } else {
+        printf("Normal Usage\n");
+    }
+
+    return 0;
+}
