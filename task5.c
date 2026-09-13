4714159fdfa482cd72094b774a7acd4f08ae6a97
Muhammad Umair Kamal <umairkamal2007@gmail.com>
2026-09-14 00:39:47 +0500

    Add files via upload

---
+++ task5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Enter choice (1-4): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("Burger\n");
+            break;
+        case 2:
+            printf("Pizza\n");
+            break;
+        case 3:
+            printf("Biryani\n");
+            break;
+        case 4:
+            printf("Sandwich\n");
+            break;
+        default:
+            printf("Invalid Choice\n");
+            break;
+    }
+
+    return 0;
+}
